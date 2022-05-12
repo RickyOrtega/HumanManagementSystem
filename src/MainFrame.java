6c9d5445e7c66d53baf288a5d177a39a1f216390
RickyOrtega <ortegavitalricky@gmail.com>
2022-05-12 01:26:11 -0500

    LogIn en Java

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -5,8 +5,32 @@
 *
 * */
 
-public class Main {
+import panels.LogIn;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
     public static void main(String[] args) {
 
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension dim = tk.getScreenSize();
+        LogIn logIn = new LogIn();
+        Image icono = tk.getImage("src/resources/img/icono.png");
+
+        JFrame mainF = new JFrame();
+        mainF.setTitle("Humansys");
+        mainF.setDefaultLookAndFeelDecorated(true);
+        mainF.add(logIn.panelIzquierdo());
+        mainF.add(logIn.panelDerecho());
+        mainF.setLayout(null);
+        mainF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainF.setVisible(true);
+        mainF.setResizable(false);
+        //Relación de aspecto del frame principal: 4:3
+        mainF.setSize(720, 480);
+        mainF.setLocationRelativeTo(null);
+
+        mainF.setIconImage(icono);
     }
 }
--- src/panels/PanelMod.java
+++ src/panels/PanelMod.java
@@ -1,2 +1,26 @@
-package panels;public class PanelMod {
+package panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelMod extends JPanel{
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        this.setLayout(null);
+        this.setSize(432,480);
+        this.setBackground(new Color(26, 76, 123));
+        this.setVisible(true);
+        g2.setColor(Color.white);
+//Pinta el grueso del borde
+        g2.setStroke(new BasicStroke(20));
+//Para un mejor renderizado
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.drawOval(116,116,200,200);
+//Para calcular el centro, es necesario dividir el tamaño del panel en dos y
+//restarle el valor de la mitad del tamaño del circulo
+        g2.fillArc(186,160,60,60,0,360);
+        g2.fillArc(148,230,136,136,0,180);
+        g2.fillRect(170,270,100,40);
+    }
 }
